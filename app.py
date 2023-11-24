87d85bd2136938e2edfea4489c4a7fce997b7127
Koushik Sarkar <koushikjit@gmail.com>
2023-11-25 01:30:49 +0530

    model coming bruhaha

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from transformers import pipeline
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, flask-github-render!</p>"
+    r = pipeline("sentiment-analysis")("we love you")
+    return f"<p>{r}Hello, flask-github-render!</p>"
